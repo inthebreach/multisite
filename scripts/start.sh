00c6d8bd52be2db1a881fa277829f0edf60cc8db
Jared Leonguerrero <linuxpir8@gmail.com>
2024-09-17 16:48:09 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ sudo service mysql start
 sudo service apache2 start
 
 # Await Gitpod port opening and output the WordPress site URL
-gp await-port 8080 && echo "WordPress is running on http://localhost:8080"
+gp await-port 8080 && gp preview $(gp url 8080)
