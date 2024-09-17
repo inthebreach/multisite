ba4a71bb5983b74f9631868a09997c790b230c68
Jared Leonguerrero <linuxpir8@gmail.com>
2024-09-17 16:41:05 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 # Start MySQL service
 sudo service mysql start || sudo /etc/init.d/mysql start
 
-# Create MySQL database for WordPress
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+# Create MySQL database for WordPress using the "adminroot" password
+mysql -u root -p'adminroot' -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
 # Download WP-CLI for WordPress installation
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -12,10 +12,10 @@ chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
 # Download WordPress
-wp core download --path=/workspace/wordpress
+wp core download --path=/workspace/multisite
 
 # Set up wp-config.php with database details
-wp config create --dbname=wordpress --dbuser=root --dbhost=localhost --path=/workspace/wordpress
+wp config create --dbname=wordpress --dbuser=root --dbpass='adminroot' --dbhost=localhost --path=/workspace/multisite
 
 # Install WordPress Multisite using environment variables for admin credentials
 wp core multisite-install --url="http://localhost:8080" \
@@ -23,4 +23,4 @@ wp core multisite-install --url="http://localhost:8080" \
   --admin_user="${WP_ADMIN_USER}" \
   --admin_password="${WP_ADMIN_PASSWORD}" \
   --admin_email="${WP_ADMIN_EMAIL}" \
-  --path=/workspace/wordpress
+  --path=/workspace/multisite
