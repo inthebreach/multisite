baf5ab383186fc299757db5a94280fcc8c7eb10d
Jared Leonguerrero <linuxpir8@gmail.com>
2024-09-17 19:10:07 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,11 @@
 # Start MySQL service, using both options for compatibility
 sudo service mysql start || sudo /etc/init.d/mysql start
 
-# Start Apache service
-sudo service apache2 start
+# Start PHP-FPM service
+sudo service php8.1-fpm start
 
-# Wait for Gitpod to open port 8080 and preview the site
-gp await-port 8080 && gp preview $(gp url 8080)
+# Start Nginx service
+sudo service nginx start
+
+# Wait for Gitpod to open port 80 and preview the site
+gp await-port 80 && gp preview $(gp url 80)
